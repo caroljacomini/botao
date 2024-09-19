ac19d3ac365235c85ea089043863c5a10d0c5dd9
caroljacomini <carol.jacomini@gmail.com>
2024-09-18 21:50:32 -0300

    Add files via upload

---
+++ js/scriptbotao.js
@@ -0,0 +1,21 @@
+//pega elementos do botão sim e não
+let botaoNao = document.getElementById("nao");
+let botaoSim = document.getElementById("sim");
+let mensagemErro = document.getElementById("erro");
+
+//função que move o botão não para uma nova posição **mouseover**
+botaoNao.addEventListener("click", function() {
+//move o botão NÃO aleatoriamente na tela
+let posX = Math.random() * (window.innerWidth - botaoNao.offsetWidth) ;
+let posY = Math.random() * (window.innerHeight - botaoNao.offsetHeight) ;
+botaoNao.style.position = "absolute";
+botaoNao.style.left = posX + "px";
+botaoNao.style.top = posY + "px";
+//exibe mensagem de erro
+mensagemErro.style.display = "block";
+});
+//mantém o botão SIM fixo
+botaoSim.addEventListener("click", function(){
+alert("ótima escolha! Minha chave pix é o número do celular!")
+});
+
